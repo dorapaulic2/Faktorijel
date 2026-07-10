64198c0a1b96a18117c08dc6c358acb4329d892a
Dora Paulic <dora.paulic2@hotmail.com>
2026-07-10 19:39:57 +0200

    faktorjel

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String... args) {
+        int broj = 5;
+        int faktorjel = 0;
+
+
+        for (int i =0; i <= broj; i++) {
+
+            faktorjel *= i;
+
+        }
+        System.out.println("Faktorjel =" + faktorjel);
+
+
+
+    }
+}
+
